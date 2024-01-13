4288033f0d48fc39ff937eccc214b3d2a32b27a5
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-01-13 15:35:49 +0900

    이름변경2

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         JonghoJang Edit <code>src/App.tsx</code> and save to reload.
+         JonghoJang2222ㅇㅇ Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
