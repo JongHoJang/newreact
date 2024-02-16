ac6e349b708bd1ae2d128daf4018657c7afd4d20
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-02-16 13:18:15 +0900

    api추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 // import './App.css';
 import Login from "./pages/Login";
 import MainPage from "./pages/MainPage/MainPage"
@@ -8,9 +8,20 @@ import {useState} from "react";
 import TotallList from "./pages/MainPage/TotallList";
 import AList from "./pages/MainPage/AList";
 import BList from "./pages/MainPage/BList";
+import axios from "axios";
 
 
 function App() {
+    const getAPI =  async () => {
+        axios.get('http://192.168.219.71/create_user.php')
+            .then(response => {
+                console.log(response.data)
+            })
+    }
+
+    useEffect(() => {
+        getAPI()
+    }, [])
 
     return (
         <div className="App">
